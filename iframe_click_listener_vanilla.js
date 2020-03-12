dc5f8673b1d7b5a4a2de188178eca63fbf5ccf78
Brian Norman <briannorman9@gmail.com>
2020-03-11 19:18:17 -0600

    Update iframe_click_listener_vanilla.js

--- iframe_click_listener_vanilla.js
+++ iframe_click_listener_vanilla.js
@@ -1,1 +1,23 @@
 
+var hovering = false;
+var paypalIframe = document.querySelector('iframe[title="Checkout with PayPal"]');
+
+paypalIframe.addEventListener('mouseover', function () {
+  hovering = true;
+});
+
+paypalIframe.addEventListener('mouseout', function () {
+  hovering = false;
+});
+
+window.addEventListener('blur', function () {
+  if (hovering) {
+    window.dataLayer.push({
+      'event': 'paypal_iframe_button_clicked'
+    });
+
+    setTimeout(function() {
+      window.focus();
+    }, 0);
+  }
+});
