494a1a44ed5749427acd926cb47bcb125f6922ca
Brian Norman <briannorman9@gmail.com>
2020-10-16 09:45:56 -0600

    Update dataLayer_event_logging.js

--- dataLayer_event_logging.js
+++ dataLayer_event_logging.js
@@ -1,9 +1,8 @@
-// might need to change `gtmDataLayer` to `dataLayer`
-var _gtmDataLayerPush = window.gtmDataLayer.push;
-window.gtmDataLayer.push = function (element, options) {
+var dataLayer = window.dataLayer.push;
+window.dataLayer.push = function (element, options) {
   var dlEventName = element.event;
   var logString = 'GTM dataLayer event: ' + dlEventName;
   console.log('%c' + logString, 'background-color: yellow; color: blue');
 
-  return _gtmDataLayerPush.apply(this, arguments);
+  return dataLayer.apply(this, arguments);
 };
