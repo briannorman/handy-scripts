fac85138e09e2cbed559f6521430d27387fe438f
Brian Norman <briannorman9@gmail.com>
2021-02-11 20:19:53 -0700

    Update ga_optly_integration.js

--- ga_optly_integration.js
+++ ga_optly_integration.js
@@ -1,46 +1,58 @@
 /**
- * ga_event_integration
- * @author brian.norman@crometrics.com (10/6/2020)
- * 
+ * ga_experiment_tracking
+ * @author brian.norman@crometrics.com (2/11/2021)
+ *
  * Module description
- * This module is firing an event to GA each time an Optly experiment activates.  The GA event data contains the Optimizely data for the experiment that was activated.
+ * This module sends Optimizely experiment data to Google Analytics.
+ *
+ * IMPORTANT: You must set the `PJS.googleAnalyticsProperty` variable in project.js prior to this module's import
  */
 
 import {
   log,
+  error,
   onCampaignDecided
-} from 'cromedics/base-pjs';
+} from 'base-pjs';
+if (PJS.googleAnalyticsProperty) {
+  const tracker = 'cro_optimizely';
 
-onCampaignDecided(event => {
-  const isCampaignHoldback = event.data.decision.isCampaignHoldback;
-  if (!isCampaignHoldback) {
-    const eventData = event.data;
-    const eventDecision = eventData.decision;
+  onCampaignDecided(data => {
+    const utils = window.optimizely.get('utils');
+    const eventDecision = data.decision;
+    const isCampaignHoldback = eventDecision.isCampaignHoldback;
     const experimentId = eventDecision.experimentId;
     const variationId = eventDecision.variationId;
-    const eventCampaign = eventData.campaign;
-    const experiment = eventCampaign.experiments[0];
-    const experimentName = experiment.name;
-    const variations = experiment.variations;
 
-    for (let i = 0; i < variations.length; i++) {
-      const thisVariation = variations[i];
-      if (variationId === thisVariation.id) {
-        const variationName = thisVariation.name;
-        const categoryString = 'Optimizely';
-        const actionString = `ExperimentID: ${experimentId} | ExperimentName: ${experimentName}`;
-        const labelString = `VariationID: ${variationId} | VariationName: ${variationName}`;
+    if (!isCampaignHoldback && experimentId && variationId) {
+      const eventCampaign = data.campaign;
+      const experiment = eventCampaign.experiments[0];
+      const experimentName = experiment.name;
+      const variations = experiment.variations;
 
-        ;
-        (function pollForGA() {
-          if (window.ga === undefined || typeof window.ga !== 'function') {
-            if (document.readyState !== 'complete') setTimeout(pollForGA, 100);
-          } else {
-            window.ga('send', 'event', categoryString, actionString, labelString);
-            log('Optimizely experiment data sent to GA:', actionString + ' | ' + labelString);
-          }
-        })();
+      for (let i = 0; i < variations.length; i++) {
+        const thisVariation = variations[i];
+        if (variationId === thisVariation.id) {
+          const eventCategory = 'Optimizely';
+          const eventAction = `${experimentId} | ${experimentName}`;
+          const eventLabel = `${variationId} | ${thisVariation.name}`;
+          utils.waitUntil(() => !!(window.ga && window.ga.getByName)).then(() => {
+            // Define our tracker if it hasn't been already.
+            if (!window.ga.getByName(tracker)) {
+              window.ga("create", PJS.googleAnalyticsProperty, {
+                name: tracker,
+                cookieDomain: "auto"
+              });
+            }
+
+            window.ga(`${tracker}.send`, 'event', eventCategory, eventAction, eventLabel, {
+              nonInteraction: 1 // non-interaction hit | true
+            });
+            log(`Event Category: ${eventCategory} | Event Action: ${eventAction} | Event Label: ${eventLabel}`);
+          }).catch(error);
+        }
       }
     }
-  }
-});
+  });
+} else {
+  error('PJS.googleAnalyticsProperty must be set in project.js prior to importing this module.');
+}
