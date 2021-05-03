cca66d9039ca877acdd6f7a2f382c8ac42a3e428
Brian Norman <briannorman9@gmail.com>
2021-05-03 15:41:40 -0600

    Update heap_track_monkeypatch.js

--- heap_track_monkeypatch.js
+++ heap_track_monkeypatch.js
@@ -1,10 +1,12 @@
 function log(logString) {
   console.log('%c' + logString, 'background-color: black; color: yellow');
 }
+
 if (window.heap.track) {
   var _track = window.heap.track;
-  window.heap.track = function (element, options) {
-    log('HEAP EVENT: Element: ' + element + ' | Options: ' + JSON.stringify(options));
+  window.heap.track = function (eventName, eventProperties) {
+    log('Heap eventName', eventName);
+    log('Heap eventProperties: ', eventProperties);
     return _track.apply(this, arguments);
   };
 }
