cbdc6537f7fe8dcd551e55864bd4ac35351318b9
Brian Norman <briannorman9@gmail.com>
2022-02-01 11:11:17 -0700

    Update simple_polling.js

--- simple_polling.js
+++ simple_polling.js
@@ -1,6 +1,12 @@
-;(function poll() {
+var safetyNet = 0;
+function poll() {
+	// break if we haven't found the thing after 2.5 seconds
+	if (safetyNet++ > 50) return;
+
     var el = document.querySelector('.my-selector');
     if (!el) return setTimeout(poll, 50);
 
     el.textContent = 'cheeseburger';
-})();
+};
+
+poll();
